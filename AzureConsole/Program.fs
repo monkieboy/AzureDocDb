3a77594986afb95bcbd920de0f406203a5b26705
Mark Gray <markgray.online@googlemail.com>
2016-04-27 21:14:47 +0100

    first local commit

---
+++ AzureConsole/Program.fs
@@ -0,0 +1,47 @@
+﻿module DbTesting.Test
+
+open System
+open System.Linq
+open System.Net
+open System.Threading.Tasks
+open Newtonsoft.Json
+open Microsoft.Azure.Documents
+open Microsoft.Azure.Documents.Client
+
+let writeToConsoleAndPromptToContinue (msg:string) =
+  printfn "%+A" msg
+  printfn "Press any key to continue ..."
+  let key = Console.ReadKey ()
+  ()
+
+// ADD THIS PART TO YOUR CODE
+let createDatabaseIfNotExists (client:DocumentClient) databaseName =
+  try
+    let readDb =
+      client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(databaseName)) |> Async.AwaitTask
+      
+
+    writeToConsoleAndPromptToContinue ("Found: " + databaseName)
+    readDb
+  with
+  | :? DocumentClientException ->
+    let db = Database() 
+    db.Id <- databaseName
+    let createDb = client.CreateDatabaseAsync(db) |> Async.AwaitTask
+
+    writeToConsoleAndPromptToContinue ("Created: " + databaseName)
+    createDb
+            
+let getStartedDemo e (p:string) =
+  let client = new DocumentClient(Uri(e), p)
+  let resp = createDatabaseIfNotExists client "JobDb" |> Async.RunSynchronously
+  resp, client
+
+
+
+[<EntryPoint>]
+let main args =
+  let endpoint = "https://1400501014.documents.azure.com:443/"
+  let primaryKey = "BkdWiGiAc8Rg75s9GeGlM5NXAvkdDTdCnydTQeACl3qEoIHYemtaGuamZKUse34pd3fIEnwsSbvdccB5l0Ynlw=="
+  let resp, client = getStartedDemo endpoint primaryKey
+  0
